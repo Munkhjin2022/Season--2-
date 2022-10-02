5a095b34e120e376d2beb90ede8a988920731dfd
Munkhjin2022 <114359135+Munkhjin2022@users.noreply.github.com>
2022-10-02 15:47:59 -0500

    Add files via upload

---
+++ ps2p2.py
@@ -0,0 +1,11 @@
+#comment input
+name = str(input("Enter last name"))
+h = float(input("Enter hours"))
+r = float(input("Enter pay rate"))
+
+#process phase
+p = h * r
+
+#output
+print("Hello", name)
+print("The gross pay is ", p)
