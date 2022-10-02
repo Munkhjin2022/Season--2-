5a6b34b984cc77d997d6754e813bc0132b6cc3b3
Munkhjin2022 <114359135+Munkhjin2022@users.noreply.github.com>
2022-10-02 15:54:36 -0500

    Add files via upload

---
+++ ps2p4.py
@@ -0,0 +1,9 @@
+#comment input
+name = str(input("Please enter last name "))
+c = float(input("How many credits do you taken? "))
+
+#process phase
+t = (250 * c) + 100
+
+#output
+print("The tuition is $250 per credit hours and added a $100 for lab fee, total is ", t)
