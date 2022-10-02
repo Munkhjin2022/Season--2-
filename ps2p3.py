2f1aa4ac7a9a41b24378727a4630d1e5474e1e99
Munkhjin2022 <114359135+Munkhjin2022@users.noreply.github.com>
2022-10-02 15:52:35 -0500

    Add files via upload

---
+++ ps2p3.py
@@ -0,0 +1,11 @@
+#comment input
+L = float(input("Please enter the length of a rectangle "))
+W = float(input("Please enter the width pf a rectangle "))
+
+#process phase
+a = L * W
+c = (2 * L) + (2 * W)
+
+#output
+print("The area is ", a)
+print("The circumference ", c)
